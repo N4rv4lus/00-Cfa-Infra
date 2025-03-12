ac5cf9db5c3c52a4878af2d3615a89514a7937c6
N4rv4lus <h22l5imny9s7cit@gmail.com>
2025-03-12 17:40:50 +0100

    Move Windows to 8th place + add small python programms

---
+++ 07-python/math-game.py
@@ -0,0 +1,62 @@
+import random
+
+NOMBRE_MIN = (1)
+NOMBRE_MAX = (100)
+NB_QUESTIONS = 10
+
+#calculez : 1 + 5
+
+def poser_question():
+    a = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+    b = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+    o = random.randint(0, 1)
+    while a == b:
+        b=random.randint(NOMBRE_MIN, NOMBRE_MAX)
+    operateur_str = "+"
+    if o == 1:
+        operateur_str = "*"
+    reponse_str = input(f"Calculez: {a} {operateur_str} {b} = ")
+    reponse_int = int(reponse_str)
+    calcul = a+b
+    if o == 1:
+        calcul = a*b
+    if reponse_int == calcul:
+       # print("Réponse correcte")
+        return True
+    #else:
+       # print("Réponse Incorrect")
+    return False
+
+""" TROUVER SOLO EHEHEHEH
+while NB_QUESTIONS > 0:
+    print(f"Question numéro {NUM_QUESTION} :")
+    poser_question()
+    NUM_QUESTION += 1
+    NB_QUESTIONS -= 1"""
+nb_points = 0
+for i in range(0, NB_QUESTIONS):
+    print(f"Question n* {i+1} sur {NB_QUESTIONS}:")
+    if poser_question():
+        print("Réponse correcte")
+        nb_points += 1
+    else:
+        print("Réponse incorrecte")
+    print()
+
+#votre note
+print(f"Votre note est : {nb_points}/{NB_QUESTIONS}")
+
+moyenne = int(NB_QUESTIONS/2)
+
+if nb_points == NB_QUESTIONS:
+    print("Excellent !")
+elif nb_points == 0:
+    print("Révisez vos maths !")
+elif nb_points < moyenne:
+    print(f"Peut mieux faire.")
+elif nb_points > moyenne:
+    print("Pas mal")
+
+
+
+
